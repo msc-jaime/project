7e5ef14c85105a239746e52482c74a78ac489211
FIRST_NAME LAST_NAME <msc.jaime@outlook.com>
2023-03-07 20:00:53 -0500

    deploy s12

---
+++ example1/js/tarea.js
@@ -0,0 +1,14 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+    console.log("se cambio el parrafo");
+}
+
+function getResource() {
+
+}
+
+function nombreFuncion(x) { 
+    return x * 5 
+}
+
+console.log(nombreFuncion(10))
